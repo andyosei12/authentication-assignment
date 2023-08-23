c1401d98ef2bd335b661eeebc23b867c6048c9cd
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-08-23 10:02:42 +0000

    feat-add user authentication

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 
 const inventoryRouter = require('./routes/inventory');
-const usersRouter = require('./routes/user');
+const userRouter = require('./routes/user');
+const { checkAuth } = require('./middlewares/user');
 
 const app = express();
 app.use(express.json());
 
 const port = 8000;
 
-app.use('/inventory', inventoryRouter);
-app.use('/users', usersRouter);
+app.use('/inventory', checkAuth, inventoryRouter);
+app.use('/users', userRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- middlewares/user.js
+++ middlewares/user.js
@@ -1,12 +1,35 @@
 const { getUsers } = require('../db/user');
 
-const checkUsername = async (req, res, next) => {
+const findUser = async (key, value) => {
   const users = await getUsers();
-  const isExistingUser = users.find(
-    (user) => user.username === req.body.username
-  );
-  console.log(isExistingUser);
-  if (isExistingUser) {
+  const existingUser = users.find((user) => user[key] === value);
+  return existingUser;
+};
+
+const checkAuth = async (req, res, next) => {
+  const apiKey = req.headers.api_key;
+  if (!apiKey) {
+    return res.status(401).json({
+      data: null,
+      error: 'Api key missen',
+    });
+  }
+
+  //   check if api key exists in db;
+  const user = await findUser('apiKey', apiKey);
+  if (!user) {
+    return res.status(404).json({
+      data: null,
+      error: 'Invalid api key',
+    });
+  }
+  req.user = user;
+  next();
+};
+
+const checkUsername = async (req, res, next) => {
+  const user = await findUser('username', req.body.username);
+  if (user) {
     return res.status(400).json({
       data: null,
       error: 'Username already exists.',
@@ -15,6 +38,31 @@ const checkUsername = async (req, res, next) => {
   next();
 };
 
+const checkIsNormalUser = async (req, res, next) => {
+  const user = req.user;
+  if (user.role !== 'user') {
+    return res.status(403).json({
+      data: null,
+      error: 'Unauthorized access',
+    });
+  }
+  next();
+};
+
+const checkIsAdmin = async (req, res, next) => {
+  const user = req.user;
+  if (user.role !== 'admin') {
+    return res.status(403).json({
+      data: null,
+      error: 'Unauthorized access',
+    });
+  }
+  next();
+};
+
 module.exports = {
   checkUsername,
+  checkAuth,
+  checkIsNormalUser,
+  checkIsAdmin,
 };
