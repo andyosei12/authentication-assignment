5c98fdd278f17aeac1d9491031c4fc3ad7fbc7c1
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-08-23 10:36:06 +0000

    check auth in getting users

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { createUser, getAllUsers } = require('../controllers/user');
-const { checkUsername } = require('../middlewares/user');
+const {
+  checkUsername,
+  checkIsAdmin,
+  checkAuth,
+} = require('../middlewares/user');
 
 const router = express.Router();
 
 router.post('/', checkUsername, createUser);
-router.get('/', getAllUsers);
+router.get('/', checkAuth, checkIsAdmin, getAllUsers);
 
 module.exports = router;
